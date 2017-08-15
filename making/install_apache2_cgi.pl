4de66f9b98462d963fb656b1109c66e6a505534b
kimbom <springnode@gmail.com>
2017-08-15 21:16:08 +0900

    apache install script

--- making/install_apache2_cgi.pl
+++ making/install_apache2_cgi.pl
@@ -12,22 +12,51 @@ use Net::Address::IP::Local;
 #Check root
 die "This scripr must be run as root" if($<!=0);
 
+print "root dir : ";
+my $root_dir=<STDIN>;
+chomp($root_dir);
 print "email : ";
 my $email=<STDIN>;
 chomp($email);
 my $ip_address=Net::Address::IP::Local->public_ipv4;
 
-
-#system "apt-get install apache2 -y";
+if(0){#===================================
+system "apt-get install apache2 -y";
 my $doc=do{  
     local $/=undef;
     open FP,"<","/etc/apache2/sites-enabled/000-default.conf" or die "$!\n";
     <FP>;
 };
+#change ServerName
+$doc=~s/#ServerName www\.example\.com/ServerName $ip_address/;
+#change root directory
+$doc=~s/\/var\/www\/html/\/var\/www\/$root_dir/;
+#change email
+my $append_element="
+	ScriptAlias /$root_dir/ /var/www/$root_dir/  
+	<Directory \"/var/www/$root_dir\">  
+                AllowOverride None
+                Options  +ExecCGI -MultiViews +SymLinksIfOwnerMatch
+                AddHandler cgi-script .cgi .pl
+                Order allow,deny
+                Allow from all
+	</Directory>
 
-say $ip_address;
 
-############
-#  IP and email setting in 000-default.conf file.
-#
-########
+";
+$doc=~s/webmaster\@localhost/$email\n\n$append_element/;
+open FP,"<","/etc/apache2/sites-enabled/000-default.conf";
+print FP $doc;
+close FP;
+}#===========================================================
+my $doc2=do{
+    local $/=undef;
+    open FP,"<","/etc/apache2/apache2.conf" or die "$!\n";
+    <FP>;
+};
+say $doc2;
+$doc2=~s/#ServerRoot/ServerRoot/;
+
+#/etc/apache2/apache2.conf line 164 replace
+say $doc2;
+
