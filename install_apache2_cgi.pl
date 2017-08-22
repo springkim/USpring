2c08df27df21426d7c02007caba57ba3faa0a0d7
kimbom <springnode@gmail.com>
2017-08-22 20:03:18 +0900

    install_apache2 for perl cgi

--- install_apache2_cgi.pl
+++ install_apache2_cgi.pl
@@ -5,6 +5,7 @@
 #  Created by kimbom on 2017. 8. 5...
 #  Copyright 2017 Sogang CVIP lab. All rights reserved.
 #
+#system "cpan install Net::Address::IP::Local";
 use strict;
 use warnings;
 use feature qw(say);
@@ -20,7 +21,6 @@ my $email=<STDIN>;
 chomp($email);
 my $ip_address=Net::Address::IP::Local->public_ipv4;
 
-if(0){#===================================
 system "apt-get install apache2 -y";
 my $doc=do{  
     local $/=undef;
@@ -45,10 +45,12 @@ my $append_element="
 
 ";
 $doc=~s/webmaster\@localhost/$email\n\n$append_element/;
-open FP,"<","/etc/apache2/sites-enabled/000-default.conf";
+open FP,">","/etc/apache2/sites-enabled/000-default.conf" or die "$!\n";
 print FP $doc;
 close FP;
-}#===========================================================
+
+die;
+
 my $doc2=do{
     local $/=undef;
     open FP,"<","/etc/apache2/apache2.conf" or die "$!\n";
@@ -57,6 +59,30 @@ my $doc2=do{
 say $doc2;
 $doc2=~s/#ServerRoot/ServerRoot/;
 
-#/etc/apache2/apache2.conf line 164 replace
-say $doc2;
 
+#/etc/apache2/apache2.conf line 164 replace
+my $append_element2="
+<Directory /var/www/$root_dir/>  
+         Options FollowSymLinks ExecCGI MultiViews
+         AddHandler cgi-script .cgi .pl
+         AllowOverride None
+         Require all granted
+</Directory>  
+";
+$doc2=~s/<Directory \/var\/www\/>.+<\/Directory>/$append_element2/s;
+$doc2.="\nServerName http://localhost";
+
+open FP,">","/etc/apache2/apache2.conf" or die "$!\n";
+print FP $doc2;
+close FP;
+
+system "a2enmod cgi";
+#system "service apache2 restart";
+system "apt-get install libcgi-session-perl -y";
+print "
+===============================================================
+모든 설치가 끝났습니다.
+마지막으로 콘솔창에서 아래의 명령어로 apache2 를 재시작 하십시오.
+service apache2 restart
+================================================================
+";
