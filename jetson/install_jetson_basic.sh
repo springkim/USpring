c57973b3e4447ea2abea3fdd8e50495ee7e974dd
kimbom <springnode@gmail.com>
2017-12-20 21:11:10 +0900

    jetson final

--- jetson/install_jetson_basic.sh
+++ jetson/install_jetson_basic.sh
@@ -1,8 +1,7 @@
 sudo apt-get install fcitx fcitx-hangul -y
 
-sudo apt-get install language-pack-en language-pack-en-base language-pack-gnome-en language-pack-gnome-en-base
-sudo apt-get install language-pack-ko language-pack-gnome-ko language-pack-ko-base language-pack-gnome-ko-base
+sudo apt-get install language-pack-en language-pack-en-base language-pack-gnome-en language-pack-gnome-en-base -y
+sudo apt-get install language-pack-ko language-pack-gnome-ko language-pack-ko-base language-pack-gnome-ko-base -y
 
-sudo apt-get install $(check-language-support -l en)
-sudo apt-get install $(check-language-support -l ko)
-#check-language-support -l kos
+sudo apt-get install $(check-language-support -l en) -y
+sudo apt-get install $(check-language-support -l ko) -y
