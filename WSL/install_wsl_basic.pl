53e766396bb60b525a807d0cf01a13bbde76860d
springkim <springnode@gmail.com>
2018-03-12 09:43:34 +0900

    wsl

--- WSL/install_wsl_basic.pl
+++ WSL/install_wsl_basic.pl
@@ -12,38 +12,23 @@ use feature qw(say);
 #Check root
 die "Please run as not superuser" if($<==0);
 system "sudo apt-get update";
-#setup WSL for GUI.
-system "sudo systemd-machine-id-setup";
-system "sudo dbus-uuidgen";
-system "cat /etc/machine-id";
-system "sudo apt -y install x11-apps xfonts-base xfonts-100dpi xfonts-75dpi xfonts-cyrillic";
-system "echo \"export DISPLAY=:0\" >> ~/.bashrc";
-#install korean font
-system "sudo apt-get install language-pack-ko fonts-unfonts-core fonts-unfonts-extra fonts-baekmuk fonts-nanum fonts-nanum-coding fonts-nanum-extra -y";
-#my $data=do{
-#    local $/=undef;
-#    <DATA>;
-#};
-#my $tmpfile="install_wsl_baslic.tmp.pl";
-#open FP,">",$tmpfile;
-#print FP $data;
-#close FP;
-#system "sudo perl $tmpfile";
-#unlink $tmpfile;
-
-#install basic program.
-system "sudo apt-get install firefox gedit nautilus file-roller -y";
-
-__DATA__
-use strict;
-use warnings;
-open(FP,'>>','/etc/default/locale');
-print FP "LANG=\"en_US.UTF-8\"\n";
-print FP "LANG=\"ko_KR.UTF-8\"\n";
-close FP;
-open(FP,'>>','/etc/environment');
-print FP "LANG=\"ko_KR.UTF-8\"\n";
-print FP "LANGUAGE=\"ko_KR:ko:en_GB:en\"\n";
-close FP;
+#setup WSL for GUI(unity)
+system "sudo apt update && sudo apt upgrade -y";
+system "sudo apt -y install ubuntu-desktop unity compizconfig-settings-manager";
+system "echo \"export DISPLAY=localhost:0\" >> ~/.bashrc";
 
 
+say "Run \"ccsm\" after \"VcXsrv\" on Windows and check below";
+say "General::Commands";
+say "General::Composite";
+say "General::Copy to texture";
+say "General::OpenGL";
+say "Desktop::Expo";
+say "Desktop::Ubuntu Unity Plugin";
+say "Image Loading::PNG";
+say "Utility::Compiz Library Toolbox";
+say "Window Management::Move Window";
+say "Window Management::Place Windows";
+say "Window Management::Resize Window";
+say "Window Management::Scale Window";
+say "Window Management::Snapping Window";
