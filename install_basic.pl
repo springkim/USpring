a63b751442c4205ab7d997b34d2cc3d625d32b93
springkim <springnode@gmail.com>
2018-04-30 04:27:40 -0700

    support 18.04

--- install_basic.pl
+++ install_basic.pl
@@ -25,15 +25,30 @@ system "git config --global user.name \"$github_name\"";
 system "git config --global user.email \"$github_email\"";
 system "git config --global credential.helper 'cache --timeout=1000000'";
 #Install theme
-system "sudo add-apt-repository ppa:snwh/pulp -y";
 system "sudo add-apt-repository ppa:numix/ppa -y";
-system "sudo add-apt-repository ppa:noobslab/themes -y";
 system "sudo apt-get update";
-system "sudo apt-get install numix-gtk-theme numix-icon-theme-circle numix-icon-theme royal-gtk-theme unity-tweak-tool tweak paper-gtk-theme -y";
+system "sudo apt-get install numix-gtk-theme numix-icon-theme-circle numix-icon-theme -y";
+my @arr=`lsb_release -a` =~ /^Release:\s+(.+)$/m;
+if($arr[0] eq "18.04"){
+	system "sudo apt install gnome-tweak-tool -y";
+	system "curl -L https://www.dropbox.com/s/3ubnlmiaxmah722/Vimix-Dark.tar.xz?dl=1 -o ~/.themes/Vimix-Dark.tar.xz";
+	system "mkdir ~/.themes" unless(-d "~/.themes");
+	system "cd ~/.themes/ && tar -xvf Vimix-Dark.tar.xz";
+	system "gsettings set org.gnome.desktop.interface gtk-theme Vimix-Dark-Doder";
+	system "gsettings set org.gnome.desktop.interface icon-theme Numix-Circle-Light";
+	#https://www.gnome-look.org/p/1013698/
+}else{	#16.04
+	system "sudo add-apt-repository ppa:snwh/pulp -y";
+	system "sudo add-apt-repository ppa:noobslab/themes -y";
+	system "sudo apt-get update";
+	system "sudo apt-get install royal-gtk-theme unity-tweak-tool tweak paper-gtk-theme -y";
+}
 #Install exfat
 system "sudo apt-get install exfat-fuse exfat-utils -y";
 #Install gparted
 system "sudo apt-get install gparted -y";
+#install dconf-tools
+system "sudo apt-get install dconf-tools -y";
 #Disable error report
 system "sudo echo enabled=0 | sudo tee /etc/default/apport";
 #Install perl libries
