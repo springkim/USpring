14228ff442326deba0b7b6d45a1f864c5d9dbd1f
springkim <springnode@gmail.com>
2020-12-08 08:14:48 +0900

    f3

--- nvidia_driver_with_cuda10.1.sh
+++ nvidia_driver_with_cuda10.1.sh
@@ -1,7 +1,13 @@
+sudo ubuntu-drivers autoinstall
 wget https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.105_418.39_linux.run
 sudo apt install gcc-8 g++-8 -y
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 9999
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 9999
 chomd +x cuda_10.1.105_418.39_linux.run
+echo "export PATH=/usr/local/cuda-10.1/bin:\$PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64:\$LD_LIBRARY_PATH" >> ~/.bashrc
+
+
+
 echo sudo ./cuda_10.1.105_418.39_linux.run
 echo uncheck nvidia driver and press install
--- theme.sh
+++ theme.sh
@@ -7,5 +7,16 @@
 #
 sudo apt-get install gdebi -y
 sudo add-apt-repository ppa:numix/ppa -y
+sudo add-apt-repository ppa:papirus/papirus -y
+
 sudo apt-get update
+
+
+
+sudo apt-get install papirus-icon-theme -y
 sudo apt-get install numix-gtk-theme numix-icon-theme-circle numix-icon-theme -y
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+echo Install font(settings/CascadiaCodePL.ttf) and apply to monospace
+
