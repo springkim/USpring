3649e6c213318a655ff5ba86c3d8ff83abf821d9
springkim <springnode@gmail.com>
2020-12-24 03:23:38 +0900

    add theme

--- theme.sh
+++ theme.sh
@@ -8,15 +8,16 @@
 sudo apt-get install gdebi -y
 sudo add-apt-repository ppa:numix/ppa -y
 sudo add-apt-repository ppa:papirus/papirus -y
-
+sudo apt-add-repository ppa:tista/adapta
 sudo apt-get update
 
 
 
 sudo apt-get install papirus-icon-theme -y
 sudo apt-get install numix-gtk-theme numix-icon-theme-circle numix-icon-theme -y
+sudo apt-get install adapta-gtk-theme -y
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
-echo Install font(settings/CascadiaCodePL.ttf) and apply to monospace
+#echo Install font(settings/CascadiaCodePL.ttf) and apply to monospace
 
