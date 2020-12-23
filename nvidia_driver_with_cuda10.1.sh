9f07d5654374bebff675c6e4c8de8af5f3a1acdc
KimBomm <springnode@gmail.com>
2020-12-24 02:56:50 +0900

    Update nvidia_driver_with_cuda10.1.sh

--- nvidia_driver_with_cuda10.1.sh
+++ nvidia_driver_with_cuda10.1.sh
@@ -5,6 +5,7 @@
 #  Created by kimbomm on 2020. 12. 03...
 #  Copyright 2020 kimbomm. All rights reserved.
 #
+sudo apt-get remove --purge '^nvidia-.*' 
 sudo ubuntu-drivers autoinstall
 wget https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.105_418.39_linux.run
 sudo apt install gcc-8 g++-8 -y
